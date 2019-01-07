cbc0986f27ff2342a7bed236bd132d829887202f
Gina <g26delarosa@gmail.com>
2019-01-06 19:28:50 -0800

    Initial Commit

---
+++ Befour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Befour
+//
+//  Created by Gina De La Rosa on 1/6/19.
+//  Copyright © 2019 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
